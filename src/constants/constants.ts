29852ae1718ac92fe506d130b11d6f5defdf67e5
Kyle Richardson <richardson.a.kyle.95@gmail.com>
2025-10-29 13:46:19 -0400

    Saving files outside of node_modules

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,8 @@
-export const TEST_DIR = `${__dirname}/../itertests`;
+import os from "os";
+import path from "path";
 
-export const CONFIG_FILE = `${TEST_DIR}/itertest.config.json`;
+export const TEST_DIR = path.join(os.homedir(), `itertest`);
+
+export const CONFIG_FILE = path.join(TEST_DIR, "itertest.config.json");
 
 export const PROGRAM_NAME = "itertest";
